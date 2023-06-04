3611645bd3ec74c8ab94f21212c957b7fa6774ec
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-06-04 14:39:39 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+var count=0
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define an API endpoint to retrieve data
+app.get('/api/data', (req, res) => {
+    // Simulated data from the backend server
+    count++
+    const data = { message: 'Hello!'+count };
+
+    setTimeout( ()=>res.json(data), 100*count);
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
